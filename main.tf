e80f0eac26281bcd84f1197eae3cc24ca5833a2c
Hector2kane <hector2kane@gmail.com>
2025-02-17 15:41:00 +0100

    ubuntu

--- main.tf
+++ main.tf
@@ -1,9 +1,3 @@
-resource "aws_instance" "idap-ubuntu" {
+resource "aws_instance" "Idap-ubuntu" {
     #instance configuration
-  ami = "i-009967b8d06728fd0"
-  instance_type = "t2.micro"
-
-    tags = {
-      Name = "Idap-ubuntu"
-    }
 }
