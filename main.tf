db8c268ad2e6d01d600e119dfea42663be91c385
Hector2kane <hector2kane@gmail.com>
2025-02-17 15:29:01 +0100

    terraform import

--- main.tf
+++ main.tf
@@ -1,3 +1,9 @@
-resource "aws_instance" "MyInstance_test" {
+resource "aws_instance" "idap-ubuntu" {
     #instance configuration
+  ami = "i-009967b8d06728fd0"
+  instance_type = "t2.micro"
+
+    tags = {
+      Name = "Idap-ubuntu"
+    }
 }
