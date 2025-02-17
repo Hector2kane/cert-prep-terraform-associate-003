bcbbc28efa56348b09bff11e9af93d85ea3dd2bd
Hector2kane <hector2kane@gmail.com>
2025-02-17 14:06:35 +0100

    modification main.tf

--- providers.tf
+++ providers.tf
@@ -1,21 +1,20 @@
 terraform {
   required_providers {
-    aws = {
-      source = "hashicorp/aws"
+    aws       = {
+      source  = "hashicorp/aws"
       version = ">= 5.31.0"
     }
   }
 }
 
 provider "aws" {
-  region = "us-east-1"
-  profile = "Helen"
+  region  = "us-west-2"
+  profile = "hector2kane"
   # Configuration options
 }
 
 provider "aws" {
-  alias = "west"
-  region = "us-west-1"
-  profile = "Helen"
+  alias   = "west"
+  region  = "us-west-1"
+  profile = "hector2kane"
 }
-
