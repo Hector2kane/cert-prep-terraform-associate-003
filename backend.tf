2d7c8dd08673ecca8a1abc5e52d8ce5ea74ad1db
Hector2kane <hector2kane@gmail.com>
2025-02-19 19:07:56 +0100

    s3 bucket name

--- backend.tf
+++ backend.tf
@@ -2,10 +2,11 @@
 # store the terraform state file in s3 and lock with dynamodb
 terraform {
   backend "s3" {
-    bucket         = "terraform-demo-s3-bucket111"
+    bucket         = "terraform-demo-s3-bucket1112"
     key            = "terraform-state-dir/terraform.tfstate"
     region         = "us-west-2"
     profile        = "default"
-    dynamodb_table = "dynamo-table1"
+
+    #dynamodb_table = "dynamo-table1"
   }
 }
