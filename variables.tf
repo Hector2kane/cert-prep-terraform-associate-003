05543fa2c3eeee56ee3a699596cba0ccc70a0796
Hector2kane <hector2kane@gmail.com>
2025-02-19 21:59:35 +0100

    tfvars

--- variables.tf
+++ variables.tf
@@ -2,8 +2,7 @@
 variable "bucket_name" {
   description = "Name for the S3 bucket"
   type        = string
-  default     = "terraform-course-demo101"
-  
+   
 }
 
 
@@ -12,9 +11,6 @@ variable "bucket_name" {
 variable "tags" {
   description     = "A map of tags for the S3 bucket"
   type            = map(string)
-  default         = {
-    Name          = "terraform-course-demo101"
-    Environment   = "Development"
-  }
+  
 }
 
