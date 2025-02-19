3bf47eee77287b274ac0a6c5c9814f81218bd41c
Hector2kane <hector2kane@gmail.com>
2025-02-19 10:26:44 +0100

    changed user

--- vpc.tf
+++ vpc.tf
@@ -4,7 +4,7 @@ module "vpc" {
   name = "my-custom-vpc"
   cidr = "10.0.0.0/16"
 
-  azs             = ["us-east-1a"]
+  azs             = ["us-west-2a"]
   private_subnets = ["10.0.1.0/24"]
   public_subnets  = ["10.0.101.0/24"]
 
