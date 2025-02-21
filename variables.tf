df417354d7b7e48a6ec71db9f51aeb5c51d2a8d4
Hector2kane <hector2kane@gmail.com>
2025-02-21 16:45:07 +0100

    tf.vars update

--- variables.tf
+++ variables.tf
@@ -2,3 +2,10 @@ variable "database_password" {
   description = "The password for the database"
   type        = string
 }
+
+variable "tags" {
+  type = map (string)
+  default = {}
+  description = "A map of tags to app"
+  
+}
